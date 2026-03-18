766cf22f9c9c87b34dd13b18f14e6edee256ca54
poldani2024 <pedro.l.oldani@gmail.com>
2026-03-18 16:24:53 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,10 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-};
+    apiKey: "AIzaSyAul9tKfKK0_5NwiYVz4VS_W5XOscaqq-I",
+    authDomain: "focus-crm-14ef2.firebaseapp.com",
+    projectId: "focus-crm-14ef2",
+    storageBucket: "focus-crm-14ef2.firebasestorage.app",
+    messagingSenderId: "995256394963",
+    appId: "1:995256394963:web:5b7c06dc9c21c75724a026",
+    measurementId: "G-SEZN0MJEPY"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
