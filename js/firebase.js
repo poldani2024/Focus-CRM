31c2eb81666a0163e7e93dba6ffdc0e4f9fce916
poldani2024 <pedro.l.oldani@gmail.com>
2026-03-21 13:44:31 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,6 @@
 // Configuración Firebase (completar)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAul9tKfKK0_5NwiYVz4VS_W5XOscaqq-I",
@@ -11,6 +11,7 @@ const firebaseConfig = {
     appId: "1:995256394963:web:5b7c06dc9c21c75724a026",
     measurementId: "G-SEZN0MJEPY"
   };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
